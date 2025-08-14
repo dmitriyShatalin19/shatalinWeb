ecb55f07e2504ce326634d76edc2146d1b55037b
dmitriyShatalin19 <shatal1d@yandex.ru>
2025-08-14 17:34:08 +0300

    обновил footer

--- script/script.js
+++ script/script.js
@@ -97,3 +97,21 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const mobileMenu = document.getElementById('burger-menu');
+    mobileMenu.classList.remove('active');
+
+    //разблокируем скролл
+    document.body.classList.remove('menu-open');
+
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      history.pushState(null, null, targetId);
+    }
+  });
+});
